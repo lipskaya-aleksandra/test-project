afebb350b02dbf82b331be4d29d8628ca3fa9f71
Aleksandra Lipskaya <Aleksandra.Lipskaya@ventionteams.com>
2024-08-09 12:25:16 +0200

    imports for RedirectToCurrentPagination component removed

--- src/common/router/config.jsx
+++ src/common/router/config.jsx
@@ -4,7 +4,6 @@ import Root, { rootLoader } from '../components/Root';
 import UsersPage from '../../users/pages/UsersPage';
 import PostsPage from '../../posts/PostsPage';
 import UserDetailPage from '../../users/pages/UserDetailPage';
-import RedirectToCurrentPagination from '../components/RedirectToCurrentPagination';
 import LoginPage from '../../users/pages/LoginPage';
 import CreateUserPage from '../../users/pages/CreateUserPage';
 import EditUserPage from '../../users/pages/EditUserPage';

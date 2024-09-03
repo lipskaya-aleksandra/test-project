bc4ac7ea037006380d8ebcdf4ba805d51fa998ad
Aleksandra Lipskaya <aleksandra.lipskaya@ventionteams.com>
2024-09-03 16:51:58 +0200

    Add form context to support password validation

--- src/auth/pages/PasswordResetPage.jsx
+++ src/auth/pages/PasswordResetPage.jsx
@@ -1,19 +1,12 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button, Container, Stack, Typography } from '@mui/material';
-import { Controller, useForm, useWatch } from 'react-hook-form';
+import { Controller, useForm, FormProvider } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 
 import PasswordInput from '../../common/components/form/PasswordInput';
 import useAlertSnackbar from '../../common/hooks/useAlertSnackbar';
 import { useResetPassword } from '../api/useResetPassword';
 import { passwordResetFormSchema } from '../utils/validation/passwordResetFormValidation';
-import {
-  AT_LEAST_ONE_DIGIT,
-  AT_LEAST_ONE_LOWERCASE_LETTER,
-  AT_LEAST_ONE_SYMBOL,
-  AT_LEAST_ONE_UPPERCASE_LETTER,
-  MIN_LENGTH,
-} from '../utils/validation/passwordValidation';
 import PasswordValidationBox from '../widgets/PasswordValidationBox';
 
 const textInputProps = {
@@ -31,7 +24,7 @@ export default function PasswordResetPage() {
   const navigate = useNavigate();
   const displaySnackbar = useAlertSnackbar();
 
-  const { control, handleSubmit } = useForm({
+  const formMethods = useForm({
     defaultValues: {
       password: '',
       confirmedPassword: '',
@@ -52,17 +45,6 @@ export default function PasswordResetPage() {
     }
   };
 
-  const password = useWatch({
-    control,
-    name: 'password',
-  });
-
-  const hasOneDigit = AT_LEAST_ONE_DIGIT.test(password);
-  const hasOneLowercaseLetter = AT_LEAST_ONE_LOWERCASE_LETTER.test(password);
-  const hasOneUppercaseLetter = AT_LEAST_ONE_UPPERCASE_LETTER.test(password);
-  const hasOneSymbol = AT_LEAST_ONE_SYMBOL.test(password);
-  const hasMinLength = password.length >= MIN_LENGTH;
-
   return (
     <Stack
       sx={{
@@ -73,77 +55,73 @@ export default function PasswordResetPage() {
         justifyContent: 'center',
       }}
     >
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Container maxWidth="xs">
-          <Typography fontWeight={300} fontSize={24} textAlign="center">
-            Reset password
-          </Typography>
+      <FormProvider {...formMethods}>
+        <form onSubmit={formMethods.handleSubmit(onSubmit)}>
+          <Container maxWidth="xs">
+            <Typography fontWeight={300} fontSize={24} textAlign="center">
+              Reset password
+            </Typography>
 
-          <Controller
-            name="password"
-            control={control}
-            render={({
-              field: { ref, ...fieldProps },
-              fieldState: { error },
-            }) => (
-              <PasswordInput
-                error={!!error}
-                helperText={error?.message}
-                label="Password"
-                {...fieldProps}
-                {...textInputProps}
-                inputRef={ref}
-              />
-            )}
-          />
+            <Controller
+              name="password"
+              control={formMethods.control}
+              render={({
+                field: { ref, ...fieldProps },
+                fieldState: { error },
+              }) => (
+                <PasswordInput
+                  error={!!error}
+                  helperText={error?.message}
+                  label="Password"
+                  {...fieldProps}
+                  {...textInputProps}
+                  inputRef={ref}
+                />
+              )}
+            />
 
-          <PasswordValidationBox
-            hasOneDigit={hasOneDigit}
-            hasOneLowercaseLetter={hasOneLowercaseLetter}
-            hasOneUppercaseLetter={hasOneUppercaseLetter}
-            hasOneSymbol={hasOneSymbol}
-            hasMinLength={hasMinLength}
-          />
+            <PasswordValidationBox />
 
-          <Controller
-            name="confirmedPassword"
-            control={control}
-            render={({
-              field: { ref, ...fieldProps },
-              fieldState: { error },
-            }) => (
-              <PasswordInput
-                error={!!error}
-                helperText={error?.message}
-                label="Confirm password"
-                {...fieldProps}
-                {...textInputProps}
-                inputRef={ref}
-              />
-            )}
-          />
+            <Controller
+              name="confirmedPassword"
+              control={formMethods.control}
+              render={({
+                field: { ref, ...fieldProps },
+                fieldState: { error },
+              }) => (
+                <PasswordInput
+                  error={!!error}
+                  helperText={error?.message}
+                  label="Confirm password"
+                  {...fieldProps}
+                  {...textInputProps}
+                  inputRef={ref}
+                />
+              )}
+            />
 
-          <Stack direction="row" justifyContent="space-between" mt={1}>
-            <Button
-              onClick={() => {
-                navigate(-1);
-              }}
-              sx={{ '&:focus': { outline: 'none' } }}
-              variant="outlined"
-            >
-              Cancel
-            </Button>
+            <Stack direction="row" justifyContent="space-between" mt={1}>
+              <Button
+                onClick={() => {
+                  navigate(-1);
+                }}
+                sx={{ '&:focus': { outline: 'none' } }}
+                variant="outlined"
+              >
+                Cancel
+              </Button>
 
-            <Button
-              sx={{ '&:focus': { outline: 'none' } }}
-              onClick={handleSubmit(onSubmit)}
-              variant="contained"
-            >
-              Submit
-            </Button>
-          </Stack>
-        </Container>
-      </form>
+              <Button
+                sx={{ '&:focus': { outline: 'none' } }}
+                onClick={formMethods.handleSubmit(onSubmit)}
+                variant="contained"
+              >
+                Submit
+              </Button>
+            </Stack>
+          </Container>
+        </form>
+      </FormProvider>
     </Stack>
   );
 }

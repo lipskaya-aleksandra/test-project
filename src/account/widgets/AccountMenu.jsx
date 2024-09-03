3135a7a712f814145de62a384ce63e8f6944ea75
Aleksandra Lipskaya <aleksandra.lipskaya@ventionteams.com>
2024-09-03 17:23:25 +0200

    Fix signout button, remove comments

--- src/account/widgets/AccountMenu.jsx
+++ src/account/widgets/AccountMenu.jsx
@@ -1,6 +1,5 @@
 import { AccountCircle, ExitToApp } from '@mui/icons-material';
 import { ListItemIcon, MenuItem } from '@mui/material';
-import { Fragment } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { useSignout } from '../../auth/api/useSignout';
@@ -12,232 +11,40 @@ export default function AccountMenu({ user }) {
   const navigate = useNavigate();
 
   return (
-    <Fragment>
-      <BaseMenu
-        tooltipTitle="Open account menu"
-        id="account-menu"
-        MenuIcon={<UserInitialsLabel user={user} />}
-        iconSx={{
-          mr: 2,
-          '&:focus': { outline: 'none' },
-          '&:hover': {
-            backgroundColor: 'inherit',
-            filter: 'drop-shadow(0px 2px 8px rgba(255,255,255,0.32))',
-          },
+    <BaseMenu
+      tooltipTitle="Open account menu"
+      id="account-menu"
+      MenuIcon={<UserInitialsLabel user={user} />}
+      iconSx={{
+        mr: 2,
+        '&:focus': { outline: 'none' },
+        '&:hover': {
+          backgroundColor: 'inherit',
+          filter: 'drop-shadow(0px 2px 8px rgba(255,255,255,0.32))',
+        },
+      }}
+    >
+      <MenuItem
+        onClick={() => {
+          navigate('/users/account');
         }}
       >
-        <MenuItem
-          onClick={() => {
-            navigate('/users/account');
-          }}
-        >
-          <ListItemIcon>
-            <AccountCircle />
-          </ListItemIcon>
-          Account
-        </MenuItem>
-        <MenuItem
-          onClick={() => {
-            signout();
-          }}
-        >
-          <ListItemIcon>
-            <ExitToApp />
-          </ListItemIcon>
-          Sign out
-        </MenuItem>
-      </BaseMenu>
-      {/* <Tooltip title="Open account menu" arrow>
-        <IconButton
-          onClick={onClick}
-          aria-controls={isOpen ? 'account-menu' : undefined}
-          aria-haspopup="true"
-          aria-expanded={isOpen ? 'true' : undefined}
-          sx={{
-            mr: 2,
-            '&:focus': { outline: 'none' },
-            '&:hover': {
-              backgroundColor: 'inherit',
-              filter: 'drop-shadow(0px 2px 8px rgba(255,255,255,0.32))',
-            },
-          }}
-        >
-          <UserInitialsLabel user={user} />
-        </IconButton>
-      </Tooltip>
-      <Menu
-        anchorEl={anchorEl}
-        id="account-menu"
-        open={isOpen}
-        onClose={onClose}
-        onClick={onClose}
-        disableScrollLock
-        slotProps={{
-          paper: {
-            elevation: 0,
-            sx: {
-              overflow: 'visible',
-              filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-              mt: 1.5,
-              '& .MuiAvatar-root': {
-                width: 32,
-                height: 32,
-                ml: -0.5,
-                mr: 1,
-              },
-              '&::before': {
-                content: '""',
-                display: 'block',
-                position: 'absolute',
-                top: 0,
-                right: 14,
-                width: 10,
-                height: 10,
-                bgcolor: 'background.paper',
-                transform: 'translateY(-50%) rotate(45deg)',
-                zIndex: 0,
-              },
-            },
-          },
+        <ListItemIcon>
+          <AccountCircle />
+        </ListItemIcon>
+        Account
+      </MenuItem>
+      <MenuItem
+        onClick={() => {
+          signout();
+          navigate('/login', { replace: true });
         }}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
       >
-        <MenuItem
-          onClick={() => {
-            navigate('/users/account');
-          }}
-        >
-          <ListItemIcon>
-            <AccountCircle />
-          </ListItemIcon>
-          Account
-        </MenuItem>
-        <MenuItem
-          onClick={() => {
-            signout();
-          }}
-        >
-          <ListItemIcon>
-            <ExitToApp />
-          </ListItemIcon>
-          Sign out
-        </MenuItem> */}
-      {/* <Stack sx={{ px: 1 }}>
-               <IconButton sx={{ color: 'gray' }}>
-                 <AccountCircle sx={{ mr: 1 }} />
-                 <Typography>Account</Typography>
-               </IconButton>
-               <IconButton
-                onClick={() => {
-                  signout();
-                  navigate('/login');
-                }}
-                sx={{
-                  color: 'red',
-                  '&:focus': { outline: 'none' },
-                }}
-              >
-                <ExitToApp sx={{ mr: 1 }} />
-                <Typography>Sign out</Typography>
-              </IconButton>
-            </Stack> */}
-      {/* </Menu> */}
-      {/* <Menu
-        anchorEl={anchorEl}
-        id="account-menu"
-        open={isOpen}
-        onClose={onClose}
-        onClick={onClose}
-        slotProps={{
-          paper: {
-            elevation: 0,
-            sx: {
-              overflow: 'visible',
-              filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-              mt: 1.5,
-              '& .MuiAvatar-root': {
-                width: 32,
-                height: 32,
-                ml: -0.5,
-                mr: 1,
-              },
-              '&::before': {
-                content: '""',
-                display: 'block',
-                position: 'absolute',
-                top: 0,
-                right: 14,
-                width: 10,
-                height: 10,
-                bgcolor: 'background.paper',
-                transform: 'translateY(-50%) rotate(45deg)',
-                zIndex: 0,
-              },
-            },
-          },
-        }}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-      >
-        <MenuItem onClick={handleClose}>
-          <Avatar /> Profile
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <Avatar /> My account
-        </MenuItem>
-        <Divider />
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <PersonAdd fontSize="small" />
-          </ListItemIcon>
-          Add another account
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <Settings fontSize="small" />
-          </ListItemIcon>
-          Settings
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <Logout fontSize="small" />
-          </ListItemIcon>
-          Logout
-        </MenuItem>
-      </Menu> */}
-    </Fragment>
-    // <Dropdown>
-    //   <ClickAwayListener
-    //     onClickAway={() => {
-    //       setIsMenuOpen(false);
-    //     }}
-    //   >
-    //     <Box>
-
-    //       <Menu open={isMenuOpen} sx={{ zIndex: 5000 }} placement="top">
-    //         <Stack sx={{ px: 1 }}>
-    //           <IconButton sx={{ color: 'gray' }}>
-    //             <AccountCircle sx={{ mr: 1 }} />
-    //             <Typography>Account</Typography>
-    //           </IconButton>
-    //           <IconButton
-    //             onClick={() => {
-    //               signout();
-    //               navigate('/login');
-    //             }}
-    //             sx={{
-    //               color: 'red',
-    //               '&:focus': { outline: 'none' },
-    //             }}
-    //           >
-    //             <ExitToApp sx={{ mr: 1 }} />
-    //             <Typography>Sign out</Typography>
-    //           </IconButton>
-    //         </Stack>
-    //       </Menu>
-    //     </Box>
-    //   </ClickAwayListener>
-    // </Dropdown>
+        <ListItemIcon>
+          <ExitToApp />
+        </ListItemIcon>
+        Sign out
+      </MenuItem>
+    </BaseMenu>
   );
 }

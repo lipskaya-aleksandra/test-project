40099e4de4fe87cdf6c8ddb0f609516e46a84598
Aleksandra Lipskaya <aleksandra.lipskaya@ventionteams.com>
2024-09-03 15:08:42 +0200

    Replace mui/joy menu with mui/material menu in EditUserCell

--- src/common/components/EditActions.jsx
+++ src/common/components/EditActions.jsx
@@ -1,18 +1,22 @@
 import { Delete as DeleteIcon, Edit as EditIcon } from '@mui/icons-material';
-import { Box, IconButton, Tooltip } from '@mui/material';
+import { Box, ListItemIcon, MenuItem, Tooltip } from '@mui/material';
 
 export default function EditActions({ onDelete, onEdit }) {
   return (
     <Box>
       <Tooltip title="Edit" arrow>
-        <IconButton sx={{ ':focus': { outline: 'none' } }} onClick={onEdit}>
-          <EditIcon />
-        </IconButton>
+        <MenuItem onClick={onEdit}>
+          <ListItemIcon>
+            <EditIcon />
+          </ListItemIcon>
+        </MenuItem>
       </Tooltip>
       <Tooltip title="Delete" arrow>
-        <IconButton sx={{ ':focus': { outline: 'none' } }} onClick={onDelete}>
-          <DeleteIcon />
-        </IconButton>
+        <MenuItem onClick={onDelete}>
+          <ListItemIcon>
+            <DeleteIcon />
+          </ListItemIcon>
+        </MenuItem>
       </Tooltip>
     </Box>
   );
--- src/users/widgets/EditUserCell.jsx
+++ src/users/widgets/EditUserCell.jsx
@@ -1,5 +1,4 @@
 import { Delete, Close, MoreVert } from '@mui/icons-material';
-import { Dropdown, IconButton, Menu, MenuButton } from '@mui/joy';
 import {
   Button,
   Dialog,
@@ -7,9 +6,9 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
-  ClickAwayListener,
-  Box,
   Tooltip,
+  IconButton,
+  Menu,
 } from '@mui/material';
 import { useState, Fragment } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -17,14 +16,16 @@ import { useNavigate } from 'react-router-dom';
 import DismissButton from '../../common/components/DismissButton';
 import EditActions from '../../common/components/EditActions';
 import useAlertSnackbar from '../../common/hooks/useAlertSnackbar';
+import useMenu from '../../common/hooks/useMenu';
 import useOptimisticUpdate from '../../common/hooks/useOptimisticUpdate';
 import { useDeleteUser } from '../api/useDeleteUser';
 import useUsersTableQueryParams from '../hooks/useUsersTableQueryParams';
 
 export default function EditUserCell({ cell }) {
   const [dialogOpen, setDialogOpen] = useState(false);
-  const [menuOpen, setMenuOpen] = useState(false);
   const navigate = useNavigate();
+
+  const { anchorEl, isOpen, onClick, onClose } = useMenu();
 
   const params = useUsersTableQueryParams();
 
@@ -71,32 +72,58 @@ export default function EditUserCell({ cell }) {
 
   return (
     <Fragment>
-      <Dropdown>
-        <ClickAwayListener
-          onClickAway={() => {
-            setMenuOpen(false);
-          }}
+      <Tooltip title="Open edit menu" arrow>
+        <IconButton
+          onClick={onClick}
+          aria-controls={isOpen ? 'account-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={isOpen ? 'true' : undefined}
+          sx={{ '&:focus': { outline: 'none' } }}
         >
-          <Box>
-            <Tooltip title="Open edit menu" arrow>
-              <MenuButton
-                onClick={() => {
-                  setMenuOpen(prev => !prev);
-                }}
-                sx={{ '&:focus': { outline: 'none' } }}
-                slots={{ root: IconButton }}
-                slotProps={{ root: { variant: 'outlined', color: 'neutral' } }}
-              >
-                <MoreVert />
-              </MenuButton>
-            </Tooltip>
+          <MoreVert />
+        </IconButton>
+      </Tooltip>
 
-            <Menu open={menuOpen} placement="right-start">
-              <EditActions onDelete={onDeleteInitiated} onEdit={onEdit} />
-            </Menu>
-          </Box>
-        </ClickAwayListener>
-      </Dropdown>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={isOpen}
+        onClose={onClose}
+        onClick={onClose}
+        disableScrollLock
+        slotProps={{
+          paper: {
+            elevation: 0,
+            sx: {
+              overflow: 'visible',
+              filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+              mt: 1.5,
+              '& .MuiAvatar-root': {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+              '&::before': {
+                content: '""',
+                display: 'block',
+                position: 'absolute',
+                top: 0,
+                right: 14,
+                width: 10,
+                height: 10,
+                bgcolor: 'background.paper',
+                transform: 'translateY(-50%) rotate(45deg)',
+                zIndex: 0,
+              },
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <EditActions onDelete={onDeleteInitiated} onEdit={onEdit} />
+      </Menu>
 
       <Dialog open={dialogOpen} onClose={handleDialogClose}>
         <DialogTitle>

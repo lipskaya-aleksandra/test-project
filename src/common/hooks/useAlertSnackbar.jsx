2a434200a2230e6347646dfe8c8bc684f9494212
Aleksandra Lipskaya <aleksandra.lipskaya@ventionteams.com>
2024-09-05 13:11:05 +0200

    Fix useCallback dependecies for optimistic update, add useCallback in snackbar hook

--- src/common/hooks/useAlertSnackbar.jsx
+++ src/common/hooks/useAlertSnackbar.jsx
@@ -1,6 +1,6 @@
 import { Alert } from '@mui/material';
 import { useSnackbar } from 'notistack';
-import { useContext, createContext } from 'react';
+import { useCallback, useContext, createContext } from 'react';
 
 const SnackbarCtx = createContext({ onClose: () => {}, key: null });
 
@@ -9,30 +9,33 @@ export const useSnackbarContext = () => useContext(SnackbarCtx);
 export default function useAlertSnackbar() {
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 
-  const pushSnackbar = ({ severity, message, Action }) =>
-    enqueueSnackbar(message, {
-      anchorOrigin: {
-        vertical: 'top',
-        horizontal: 'center',
-      },
-      autoHideDuration: 5000,
-      content: (key, msg) => (
-        <Alert
-          onClose={() => {
-            closeSnackbar(key);
-          }}
-          severity={severity ?? 'success'}
-          sx={{ width: '100%' }}
-          action={
-            <SnackbarCtx.Provider value={{ key, onClose: closeSnackbar }}>
-              {Action}
-            </SnackbarCtx.Provider>
-          }
-        >
-          {msg}
-        </Alert>
-      ),
-    });
+  const pushSnackbar = useCallback(
+    ({ severity, message, Action }) =>
+      enqueueSnackbar(message, {
+        anchorOrigin: {
+          vertical: 'top',
+          horizontal: 'center',
+        },
+        autoHideDuration: 5000,
+        content: (key, msg) => (
+          <Alert
+            onClose={() => {
+              closeSnackbar(key);
+            }}
+            severity={severity ?? 'success'}
+            sx={{ width: '100%' }}
+            action={
+              <SnackbarCtx.Provider value={{ key, onClose: closeSnackbar }}>
+                {Action}
+              </SnackbarCtx.Provider>
+            }
+          >
+            {msg}
+          </Alert>
+        ),
+      }),
+    [enqueueSnackbar, closeSnackbar],
+  );
 
   return pushSnackbar;
 }
